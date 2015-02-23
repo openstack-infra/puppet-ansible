36da8fc39b6c3f32943bc739b04b29a1450abcae
Monty Taylor <mordred@inaugust.com>
2015-02-22 23:08:10 -0500

    Put the library file into the role
    Roles can contain embedded library code, which is how roles work when
    you distribute them on ansible galaxy. Put the library there and remove
    the old location to prepare for standalone modules.
    
    Change-Id: Iaa7906f71bb0a3bc255695e72b6ae061407fa2b8

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,10 +43,7 @@ class ansible (
   }
 
   file { '/etc/ansible/library':
-    ensure  => directory,
-    recurse => true,
-    source  => 'puppet:///modules/ansible/library',
-    require => File['/etc/ansible'],
+    ensure  => absent,
   }
 
   include logrotate
