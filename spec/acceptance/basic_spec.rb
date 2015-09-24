12ab892369edaf55322ead66555db4de20e0cbae
Bruno Tavares <btavare@thoughtworks.com>
2015-09-24 19:37:09 -0300

    Move test assertions closer to apply.
    As discussed on another project patches when introducing tests[1], we
    would like to keep the tests closer to the where we apply the spec.
    
    This change makes the testing structure consistent to the feedback given
    on puppet-bandersnatch discussion.
    
    [1] https://review.openstack.org/#/c/221941/
    
    Change-Id: I7d0d1dc856f03f422fa5331a98f20d5661c462be
    Co-Authored-By: Danilo Ramalho <dramalho@thoughtworks.com>

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -16,7 +16,32 @@ describe 'puppet-ansible module' do
   end
 
   it 'should be idempotent' do
-    apply_manifest(default_puppet_module, catch_failures: true)
     apply_manifest(default_puppet_module, catch_changes: true)
+  end
+
+  describe 'required python package' do
+    describe package('ansible') do
+      it { should be_installed.by('pip') }
+    end
+  end
+
+  describe 'required files' do
+    describe file('/etc/ansible/ansible.cfg') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      its(:content) { should include 'library=/usr/share/ansible' }
+    end
+
+    describe file('/usr/local/bin/puppet-inventory') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      its(:content) { should include "'_meta': {'hostvars': dict()}," }
+    end
+
+    describe file('/etc/logrotate.d/ansible') do
+      its(:content) { should include '/var/log/ansible.log' }
+    end
   end
 end
--- spec/acceptance/packages_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper_acceptance'
-
-describe 'required python package' do
-  describe package('ansible') do
-    it { should be_installed.by('pip') }
-  end
-end
