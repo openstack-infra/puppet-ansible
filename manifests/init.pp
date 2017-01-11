6449d5fe4acb8da39fa714ec1f7e7a65a2bb9324
Clark Boylan <clark.boylan@gmail.com>
2017-01-11 07:21:19 -0800

    Forcefully install latest ansible
    There is an unhappy security issue (CVE 2016 9587) that we need to
    avoid. The way to do this is to install the latest RC release for
    ansible 2.1 or ansible 2.2. Unfortunately neither of these RCs has made
    it to pypi so we have to manually target those release tarballs hosted
    by ansible.
    
    This results in replacing the puppet package resource with an exec that
    invokes pip properly to install this version. Extra unfortunately this
    means we have to currently ignore the version specifier in the parameter
    list.
    
    Change-Id: I220deb2911fba27f6d793fc0b7ba40457862b3b2

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,9 @@
 class ansible (
   $ansible_hostfile      = '/etc/ansible/hosts',
   $ansible_roles_path    = '/etc/ansible/roles',
+  # Note this version is temporarily ignored while we install non
+  # CVE 2016 9587 affected ansible via tarball because ansible doesn't
+  # put their RCs on pypi.
   $ansible_version       = '2.0.2.0',
   $retry_files_enabled   = undef,
   $retry_files_save_path = undef,
@@ -11,9 +14,11 @@ class ansible (
   include ::logrotate
   include ::pip
 
-  package { 'ansible':
-    ensure   => $ansible_version,
-    provider => openstack_pip,
+  exec { 'install-ansible-rc':
+    command  => 'pip install https://releases.ansible.com/ansible/ansible-2.2.1.0-0.3.rc3.tar.gz',
+    unless   => 'pip freeze | grep \'ansible==2.2.1.0\'',
+    path     => '/usr/local/bin:/bin:/usr/bin',
+    require  => Class['pip'],
   }
 
   if ! defined(File['/etc/ansible']) {
